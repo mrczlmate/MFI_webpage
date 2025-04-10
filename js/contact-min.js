1c93e83ec8f7f7f887bc1fc3c7f6532fbda3e26a
mrczlmate <merczel259@gmail.com>
2025-04-10 14:34:04 +0200

    remove minimized files, SEO worst with these files

--- js/contact-min.js
+++
@@ -1,5 +0,0 @@
-"use strict";document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".contact-form");e.addEventListener("submit",function(t){t.preventDefault();let n=new FormData(e),a=n.get("name"),l=n.get("email"),r=n.get("message");if(!a||!l||!r){alert("K\xe9rlek, t\xf6lts ki minden mezőt!");return}let i=encodeURIComponent("\xdczenet a varroda weboldal\xe1r\xf3l"),o=encodeURIComponent(`N\xe9v: ${a}
-Email: ${l}
-
-\xdczenet:
-${r}`);window.location.href=`mailto:merczelg@gmail.com?subject=${i}&body=${o}`})});
